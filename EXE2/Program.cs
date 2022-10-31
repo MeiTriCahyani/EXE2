a3ac84f2e521b646eddf54a21f37d754e65fb76a
TB1112 <your_mei.tri.ft21@mail.umy.ac.id>
2022-10-31 15:56:28 +0700

    menambahkan array int i

--- EXE2/Program.cs
+++ EXE2/Program.cs
@@ -9,10 +9,13 @@ namespace EXE2
     class Program
     {
         //Deklarasi array int dengan ukuran 32
-        int[] MC = new int[32];
+        int[] MT = new int[32];
 
         //deklarasi variabel int untuk menyimpan banyaknya data yang disimpan pada array
         int n;
+
+        //mendapatkan jumlah elemen untuk disimpan apada array
+        int i;
 
         static void Main(string[] args)
         {
